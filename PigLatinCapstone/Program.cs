ef451e900e6fa9e8b92a53512a4ef6995bd520ea
radeen <radeen013@gmail.com>
2021-09-30 16:02:50 -0400

    test first commit

---
+++ PigLatinCapstone/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PigLatinCapstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            //Getting user input
+            
+            Console.WriteLine("Welcome to the PigLatinator. Enter a word: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(FirstLetterSeperator(input));
+            
+        }
+
+
+        
+        public static string FirstLetterSeperator(string x)
+        {
+            string firstLetter = x.Substring(0, 1);
+            return firstLetter;
+        }
+        //public static bool VowelChecker(bool x)
+        //{
+            
+        //    if (x.(1) = "a")
+        //    {
+
+        //    }
+            
+
+        //}
+    }
+}
