3466717891223b05bbb713949f5dd6f20465376c
radeen <radeen013@gmail.com>
2021-10-01 14:26:57 -0400

    third commit

--- PigLatinCapstone/Program.cs
+++ PigLatinCapstone/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PigLatinCapstone
@@ -12,87 +13,33 @@ namespace PigLatinCapstone
             Console.WriteLine("Welcome to the PigLatinator. Enter a word: ");
             string input = Console.ReadLine();
             Console.WriteLine(ToPigLatin(input));
-            //Console.WriteLine(FirstLetterSeperator(input));
-            
+           
         }
 
-
-        public static string ToPigLatin(string sentencetext)
+        static string ToPigLatin(string sentence)
         {
-            string vowels = "AEIOUaeiou";
-            string cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
 
-            Func<string, string> toPigLatin = word =>
+            const string vowels = "aeiou";
+            List<string> pigWords = new List<string>();
+
+            foreach (string word in sentence.Split(' '))
             {
-                word = word.ToLower();
+                string firstLetter = word.Substring(0, 1);
+                string restOfWord = word.Substring(1, word.Length - 1);
+                int currentLetter = vowels.IndexOf(firstLetter);
 
-                var result = word;
-
-                Func<string, string, (string, string)> split = (w, l) =>
+                if (currentLetter == -1)
                 {
-                    var prefix = new string(w.ToArray().TakeWhile(x => l.Contains(x)).ToArray());
-                    return (prefix, w.Substring(prefix.Length));
-                };
-
-                if (!word.Any(w => cons.Contains(w)))
-                {
-                    result = word + "way";
+                    pigWords.Add(restOfWord + firstLetter + "ay");
                 }
                 else
                 {
-                    var (s, e) = split(word, vowels);
-                    var (s2, e2) = split(e, cons);
-                    result = e2 + s + s2 + "ay";
+                    pigWords.Add(word + "way");
                 }
-                return result;
-            };
-
-            return string.Join(" ", sentencetext.Split(' ').Select(x => toPigLatin(x)));
+            }
+            return string.Join(" ", pigWords);
         }
 
-
-
-        //static string ToPigLatin(string sentence)
-        //{
-
-        //    const string vowels = "AEIOUaeio";
-        //    List<string> pigWords = new List<string>();
-
-        //    foreach (string word in sentence.Split(' '))
-        //    {
-        //        string firstLetter = word.Substring(0, 1);
-        //        string restOfWord = word.Substring(1, word.Length - 1);
-        //        int currentLetter = vowels.IndexOf(firstLetter);
-
-        //        if (currentLetter == -1)
-        //        {
-        //            pigWords.Add(restOfWord + firstLetter + "ay");
-        //        }
-        //        else
-        //        {
-        //            pigWords.Add(word + "way");
-        //        }
-        //    }
-        //    return string.Join(" ", pigWords);
-        //}
-
-        //This method separates the first letter 
-        //public static string FirstLetterSeperator(string x)
-        //{
-        //    string firstLetter = x.Substring(0, 1);
-        //    return firstLetter;
-        //}
-
-        //public static bool VowelChecker(bool x)
-        //{
-
-        //    if (x.Equals = "a")
-        //    {
-
-        //    }
-
-
-
-        //}
+        
     }
 }
